c0e6f961ee64a2ed5576c1996684dd217cba2e12
Pramod <pramodballa007@gmail.com>
2024-04-06 11:22:16 +0530

    Conflict Scenario2

--- src/p2/Test2.java
+++ src/p2/Test2.java
@@ -4,6 +4,9 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("In Test2 File");
+		System.out.println("Conflict scenario 2");
+		System.out.println("branch A line 10");
+		
 	}
 
 }
